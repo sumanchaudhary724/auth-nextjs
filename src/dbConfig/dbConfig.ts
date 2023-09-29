056e61645a9d37f25e68f5d5414d3a2e5f96cc3f
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-09-30 02:51:24 +1000

    signup and login page created

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -4,8 +4,15 @@ export async function connect() {
   try {
     mongoose.connect(process.env.MONGO_URL!);
     const connection = mongoose.connection;
-    connection.on("Connected", () => {
+    connection.on("connected", () => {
       console.log("MongoDB connected successfully");
+    });
+
+    connection.on("error", (err) => {
+      console.log(
+        "MongoDB connection error. Please make sure MongoDB is running." + err
+      );
+      process.exit();
     });
   } catch (error) {
     console.log("Something goes wrong!");
