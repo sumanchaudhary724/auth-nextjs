16cf2fad963dfeb1741e85f700bc084ea8b03759
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-10-07 23:42:19 +1100

    Done  middleware to give access to the router

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -10,42 +10,53 @@ export async function POST(request: NextRequest) {
   try {
     const reqBody = await request.json();
     const { email, password } = reqBody;
-    console.log(reqBody);
 
-    //check if user exists
+    // Check if the user exists
     const user = await User.findOne({ email });
+
     if (!user) {
       return NextResponse.json(
-        { error: "User does not exit" },
+        { error: "User does not exist" },
         { status: 400 }
       );
     }
 
-    //check if password is correct
+    // Check if the password is correct
     const validPassword = await bcryptjs.compare(password, user.password);
+
     if (!validPassword) {
       return NextResponse.json({ error: "Invalid password" }, { status: 400 });
     }
 
-    //create token data
+    // Create token data
     const tokenData = {
       id: user._id,
       username: user.username,
       email: user.email,
     };
 
-    //create token
+    // Create a token
     const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
       expiresIn: "1d",
     });
+    console.log(token);
 
+    // Set the token as an HTTP-only cookie
     const response = NextResponse.json({
       message: "Login successful",
       success: true,
     });
-    response.cookies.set("token", token, { httpOnly: true });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
     return response;
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    console.error("Error:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicPath = path === "/login" || path === "/signup";
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+
+  // Allow access to '/profile' if the user has a valid token
+  if (path === "/profile" && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup"],
+};
