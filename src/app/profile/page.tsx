94a74e03ed142c30262c98e6678d0542d5ece2b7
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-10-08 00:25:41 +1100

    Updated in profile to get user details

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -19,6 +19,12 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
@@ -37,6 +43,12 @@ export default function ProfilePage() {
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Get User Details
       </button>
     </div>
   );
